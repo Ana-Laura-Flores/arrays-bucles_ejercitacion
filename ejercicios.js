b7031bb737ed6397ef79ed301addd29c927ea551
Ana Laura Flores <anadisenio@hotmail.com>
2023-03-30 12:46:11 -0300

    resolution exercise 2

--- ejercicios.js
+++ ejercicios.js
@@ -23,3 +23,21 @@ const sumarNumeros = (numeros) =>{
 console.log(sumarNumeros([5, 7, 10, 12,125, 24])) // 58
 
 
+//contiene(numero, numeros)
+//Crear una funci´ón contiene que tome como argumentos un número numero y un array de números numeros y devuelva true o false dependiendo de si dicho numero se encuentra en el array de numeros. Ejemplo:
+
+
+const contiene = (numero, arrayNumeros)=>{
+for (const num of arrayNumeros){
+    if (num === numero){
+        return true
+    }
+}
+return false
+}
+
+console.log(contiene(54, [5, 7, 99, 3, 4, 54, 2, 12])) // true
+console.log(contiene(103, [5, 7, 99, 3, 4, 54, 2, 12])) // false
+
+
+
